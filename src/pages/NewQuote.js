c66b86e1390450c6a2f89652f211abb58b9dee11
Moez <moez_zouari@hotmail.fr>
2021-06-24 14:56:26 +0100

    Implementing Programmatic Navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandlerFunction = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandlerFunction} />;
 };
